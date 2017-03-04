a40d3a8b907d3c4bf614cbeb482fd904544ef19c
Janssen <larsjanssen64@gmail.com>
2017-03-04 16:59:35 +0100

    add to interface

--- src/Repository/OrderRepositoryInterface.php
+++ src/Repository/OrderRepositoryInterface.php
@@ -42,6 +42,22 @@ interface OrderRepositoryInterface
     public function toMiddle(Model $model);
 
     /**
+     * @param Model $model1
+     * @param Model $model2
+     * @return mixed
+     */
+    public function switchModels(Model $model1, Model $model2);
+
+    /**
+     * @param Model $model
+     * @param $index1
+     * @param $index2
+     * @return mixed
+     */
+    public function switchIndexes(Model $model, $index1, $index2);
+
+
+    /**
      * @param Model $model
      *
      * @return mixed
